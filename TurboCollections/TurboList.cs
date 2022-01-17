9918d40b24457006eabf568798206863d8900c8a
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-01-17 23:56:23 +0100

    Add AddElementCanBeGet Project

--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -5,10 +5,33 @@ namespace TurboCollections
 {
     public class TurboList<T>
     {
-        public int Count => 5;
+        public int Count => items.Length;
+        private T[] items = Array.Empty<T>();
         public TurboList()
         {
             Console.WriteLine(("Hello, Turbo!"));
+        }
+
+        public void Add(T item)
+        {
+            //Resize teh Array
+            T[] newArray = new T[Count + 1];
+            for (int i = 0; i < Count; i++)
+            {
+                newArray[i] = items[i];
+            }
+            
+            //Assing the new element
+            newArray[Count] = item;
+            
+            // Assign the result to our field
+            items = newArray;
+            
+        }
+
+        public T Get(int index)
+        {
+            return items[index];
         }
     }
     
