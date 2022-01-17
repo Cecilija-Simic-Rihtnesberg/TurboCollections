54a074711b4ca7d5de37e29305a1254a6e96067c
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-01-17 22:58:58 +0100

    Add CustomerManagement Project

---
+++ CustomerManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TurboCollections;
+
+namespace CustomerManagement
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var turbolist = new TurboList<float>();
+        }
+    }
+    
+
+}
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -1,5 +1,16 @@
-﻿namespace TurboCollections;
+﻿using System;
 
-public class TurboList<T>
+namespace TurboCollections
+
 {
+    public class TurboList<T>
+    {
+        public int Count => 5;
+        public TurboList()
+        {
+            Console.WriteLine(("Hello, Turbo!"));
+        }
+    }
+    
+
 }
